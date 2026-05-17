defc60cc2e8ce49fd9c2d2cb630ed7365bb504cf
jibinsha <68502493+jibinsha@users.noreply.github.com>
2026-05-17 17:51:38 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 
 import 'leaflet/dist/leaflet.css'
 
+import 'leaflet-defaulticon-compatibility'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
